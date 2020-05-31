1f46f27e98c250e47254e1223577f08ef8c43e23
dasariravichandra <52750609+dasariravichandra@users.noreply.github.com>
2020-06-01 00:14:29 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userfromrepo = await _repo.Login(userForLoginDto.UserName.ToLower(),userForLoginDto.Password);
+            if(userfromrepo == null)
+                return Unauthorized();
+            
+            var claims = new[] 
+            {
+                new Claim(ClaimTypes.NameIdentifier, userfromrepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userfromrepo.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            var tokenhandler = new JwtSecurityTokenHandler();
+            var token = tokenhandler.CreateToken(tokenDescriptor);
+            return Ok(new {
+                token =tokenhandler.WriteToken(token)
+            });
+
+        }
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegister userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if(await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("UserName Already Exists");
+
+            var userToCreate = new User
+            {
+                UserName= userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+        
+    }
+}
---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var Values = await _context.Values.ToListAsync();
+            return Ok(Values);
+        }
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.Where(x=>x.Id == id).FirstOrDefaultAsync();
+            return Ok(value);
+        }
+    }
+}
